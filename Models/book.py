ce4a0e79573273c2174c5ec5dd2379ac1705755e
sadegh <mse737.khavari@gmail.com>
2019-10-12 19:23:26 +0330

    Add save and implicit find for books

--- Models/book.py
+++ Models/book.py
@@ -1,53 +1,131 @@
 class Book():
-    def __init__(self, data, _id = None):
+    def __init__(self, data, _id=None):
         for key, val in data.items():
             self.__dict__[key] = val
         self._id = _id
 
     def save(self):
-        #if id was None add a new line else edit existing line
+        validation_status = self.__validate()
+        if(validation_status != 'valid'):
+            print('Validation Failed:', validation_status)
+            return
+
+        if self._id == None:
+            # New Record (append)
+            with open("Data/books.txt", "a+") as file:
+                file.seek(0)
+                record_id = len(file.readlines()) + 1
+                file.write(f'{record_id}-{self}\n')
+
+        else:
+            # Existing Record (overwrite)
+            pass
+
+    def remove(self):
         pass
 
-        #validate a
-    def validation(self):
-        if len(self.ISBN) != 20:
-            raise ValueError('A very specific bad thing happened.')
-    
+    def __save_indexes(self):
+        pass
+        # validate a
+
+    def __validate(self):
+        if len(self.ISBN) != 2:
+            return 'ISBN should containt 20 chars'
+        # if le(self.)
+        return 'valid'
+
     def update_data(self, new_data):
         for key, val in new_data.items():
             self.__dict__[key] = val
 
-    #return a represntable string form for instance
+    # return a represntable string form for instance
     def __str__(self):
-        return "not implemnted"
-    
-    # returns a dict contains book attributes and values
-    @staticmethod
-    def deserialize(parameter_list):
-        pass
+        return f'BookName:{self.BookName} , Authors:{self.Authors} , Publisher:{self.Publisher} , Subjects:{self.Subjects} , PublishedYear:{self.PublishedYear} , PageNo:{self.PageNo}'
 
-    # reduce query and calll the target function
+    # returns a dict contains book attributes and values (used for quick parse in search and creating instance in updating and saving record)
     @staticmethod
-    def query(query_type, body):
-        print(query_type, body)
+    def parse_attributes(attrs_str):
+        attrs = attrs_str.split(' , ')
+        # attrs_dict = dict()
+        # for attr in attrs:
+        #     key, val = attr.split(':')[0], attr.split(':')[1]
+        #     attrs_dict[key] = val
+        return {attr.split(':')[0]: attr.split(':')[1] for attr in attrs}
+
+    # reduce query and call the target function
+    @classmethod
+    def query(cls, query_type, body):
+        if query_type == 'add':
+            cls.add(body)
+        elif query_type == 'find':
+            cls.find(body)
+        elif query_type == 'update':
+            cls.update(body)
+        elif query_type == 'remove':
+            cls.remove(body)
+        else:  # throw exepction
+            pass
 
     # add new book
-    @staticmethod
-    def add(book_str):
-        a = Book()
+    @classmethod
+    def add(cls, book_attrs):
+        book_attrs = cls.parse_attributes(book_attrs)
+        new_book = cls(data=book_attrs)
+        new_book.save()
 
     # remove book
-    @staticmethod
+    @classmethod
     def remove(isbn):
         pass
 
-    # updates a book 
-    @staticmethod
+    # updates a book
+    @classmethod
     def update():
         pass
 
     # returns a book object or null
-    @staticmethod
-    def find():
+    @classmethod
+    def find(cls, body):
+        attr, val = body.split(' by ')[1], body.split(' by ')[0]
+        results = []
+        if attr == 'BookName':
+            results = cls.find_by_name(val)
+        elif attr == 'ISBN':
+            results = cls.find_by_isbn(val)
+        elif attr == 'Authors':
+            results = cls.find_by_isbn(val)
+        else:
+            results = cls.implicit_find(attr, val)
+
+        if len(results) == 0:
+            print('Nothing Founded!')
+        else:
+            for num, record in enumerate(results, start=1):
+                print(f'{num}-{record}')
+
+    # find by any attributes WITHOUT INDEXING
+    @classmethod
+    def implicit_find(cls, attr, val):
+        results = []
+        with open("Data/books.txt", "r") as file:
+            for num, line in enumerate(file, start=1):
+                attrs_str = '-'.join(line[:-1].split('-')[1:])
+                attrs = cls.parse_attributes(attrs_str)
+                if val in attrs[attr]:
+                    results.append(cls(data=attrs, _id=num))
+        return results
+
+     # using indexes
+    @classmethod
+    def find_by_name(cls, name):
         pass
 
+    # using indexes
+    @classmethod
+    def find_by_isbn(cls, isbn):
+        pass
+
+    # using indexes
+    @classmethod
+    def find_by_authors(cls, author):
+        pass
--- mainISR.py
+++ mainISR.py
@@ -2,11 +2,11 @@ from Models.book import Book
 
 while(True):
 
-    command = input()
+    command = input("\n-->")
     command_parsed = command.split()
     model = command_parsed[1]
     query_type = command_parsed[0]
-    body = command_parsed[2:]
+    body = ' '.join(command_parsed[2:])
 
     if model == 'book':
         Book.query(query_type, body)
