50844e4143b8989a43555659b114392af5fd445e
sadegh <mse737.khavari@gmail.com>
2019-10-14 01:09:54 +0330

    Complete Validation, non indexed find, update

--- Models/book.py
+++ Models/book.py
@@ -1,16 +1,27 @@
 class Book():
+    __attributes_options = {
+        "ISBN": {"exact": 20, "unique": True, "indexed": True, "index_file": "BookISBNIndex.txt"},
+        "BookName": {"lowerbound": 1, "upperbound": 200, "indexed": True, "index_file": "BookTitleIndex.txt"},
+        "Authors": {"upperbound": 200, "indexed": True, "index_file": "BookAuthorsIndex.txt"},
+        "Publisher": {"upperbound": 200, "indexed": False},
+        "Subjects": {"upperbound": 100, "indexed": False, },
+        "PublishedYear": {"exact": 4, "indexed": False, },
+        "PageNo": {"upperbound": 4, "indexed": False, },
+    }
+
     def __init__(self, data, _id=None):
         for key, val in data.items():
             self.__dict__[key] = val
         self._id = _id
 
     def save(self):
-        validation_status = self.__validate()
+        new_record = (self._id == None)
+        validation_status = self.__validate(new_record)
         if(validation_status != 'valid'):
-            print('Validation Failed:', validation_status)
+            print('Validation Failed: ', validation_status)
             return
 
-        if self._id == None:
+        if new_record:
             # New Record (append)
             with open("Data/books.txt", "a+") as file:
                 file.seek(0)
@@ -19,19 +30,26 @@ class Book():
 
         else:
             # Existing Record (overwrite)
-            pass
+            lines = open('Data/books.txt').read().splitlines()
+            lines[self._id - 1] = f'{self._id}-{self}'
+            open('Data/books.txt', 'w').write('\n'.join(lines))
 
     def remove(self):
         pass
 
     def __save_indexes(self):
         pass
-        # validate a
 
-    def __validate(self):
-        if len(self.ISBN) != 2:
-            return 'ISBN should containt 20 chars'
-        # if le(self.)
+    def __validate(self, new_record):
+        for attr, rules in Book.__attributes_options.items():
+            if 'exact' in rules.keys() and len(self.__dict__[attr]) != rules["exact"]:
+                return f'{attr.lower()} should containt exactly {rules["exact"]} characters'
+            if 'upperbound' in rules.keys() and len(self.__dict__[attr]) > rules['upperbound']:
+                return f'{attr.lower()} should containt at most {rules["upperbound"]} characters'
+            if 'lowerbound' in rules.keys() and len(self.__dict__[attr]) < rules["lowerbound"]:
+                return f'{attr.lower()} should containt at least {rules["lowerbound"]} characters'
+            if new_record and 'unique' in rules.keys() and rules['unique'] and Book.search_handler(attr, self.__dict__[attr]):
+                return f'a book with this {attr.lower()} already exists'
         return 'valid'
 
     def update_data(self, new_data):
@@ -40,16 +58,12 @@ class Book():
 
     # return a represntable string form for instance
     def __str__(self):
-        return f'BookName:{self.BookName} , Authors:{self.Authors} , Publisher:{self.Publisher} , Subjects:{self.Subjects} , PublishedYear:{self.PublishedYear} , PageNo:{self.PageNo}'
+        return f'ISBN:{self.ISBN} , BookName:{self.BookName} , Authors:{self.Authors} , Publisher:{self.Publisher} , Subjects:{self.Subjects} , PublishedYear:{self.PublishedYear} , PageNo:{self.PageNo}'
 
     # returns a dict contains book attributes and values (used for quick parse in search and creating instance in updating and saving record)
     @staticmethod
     def parse_attributes(attrs_str):
         attrs = attrs_str.split(' , ')
-        # attrs_dict = dict()
-        # for attr in attrs:
-        #     key, val = attr.split(':')[0], attr.split(':')[1]
-        #     attrs_dict[key] = val
         return {attr.split(':')[0]: attr.split(':')[1] for attr in attrs}
 
     # reduce query and call the target function
@@ -64,7 +78,7 @@ class Book():
         elif query_type == 'remove':
             cls.remove(body)
         else:  # throw exepction
-            pass
+            print("Invalid Query")
 
     # add new book
     @classmethod
@@ -75,37 +89,45 @@ class Book():
 
     # remove book
     @classmethod
-    def remove(isbn):
+    def remove(cls, isbn):
         pass
 
     # updates a book
     @classmethod
-    def update():
-        pass
+    def update(cls, body):
+        isbn, new_data = body.split(' set ')
+        attr, val = new_data.split(' to ')
+        result = cls.search_handler('ISBN', isbn)
+        if result == []:
+            print("nothing found")
+            return
+        book = result[0]
+        book.update_data({attr: val})
+        book.save()
 
     # returns a book object or null
     @classmethod
     def find(cls, body):
-        attr, val = body.split(' by ')[1], body.split(' by ')[0]
-        results = []
-        if attr == 'BookName':
-            results = cls.find_by_name(val)
-        elif attr == 'ISBN':
-            results = cls.find_by_isbn(val)
-        elif attr == 'Authors':
-            results = cls.find_by_isbn(val)
-        else:
-            results = cls.implicit_find(attr, val)
-
+        val, attr = body.split(' by ')
+        results = cls.search_handler(attr, val)
         if len(results) == 0:
             print('Nothing Founded!')
         else:
             for num, record in enumerate(results, start=1):
                 print(f'{num}-{record}')
 
+    @classmethod
+    def search_handler(cls, attr, val):
+        results = []
+        if cls.__attributes_options[attr]['indexed']:
+            results = cls.non_indexed_search(attr, val)
+        else:
+            results = cls.non_indexed_search(attr, val)
+        return results
+
     # find by any attributes WITHOUT INDEXING
     @classmethod
-    def implicit_find(cls, attr, val):
+    def non_indexed_search(cls, attr, val):
         results = []
         with open("Data/books.txt", "r") as file:
             for num, line in enumerate(file, start=1):
@@ -117,15 +139,6 @@ class Book():
 
      # using indexes
     @classmethod
-    def find_by_name(cls, name):
-        pass
-
-    # using indexes
-    @classmethod
-    def find_by_isbn(cls, isbn):
-        pass
-
-    # using indexes
-    @classmethod
-    def find_by_authors(cls, author):
-        pass
+    def indexed_search(cls, attr, val):
+        index_file = cls.__attributes_options[attr]["index_file"]
+        return []
--- mainISR.py
+++ mainISR.py
@@ -3,10 +3,20 @@ from Models.book import Book
 while(True):
 
     command = input("\n-->")
+
+    if len(command) == 0:
+        continue
+
     command_parsed = command.split()
+    if len(command_parsed) < 3:
+        print('Invalid Command')
+        continue
+
     model = command_parsed[1]
     query_type = command_parsed[0]
     body = ' '.join(command_parsed[2:])
 
     if model == 'book':
         Book.query(query_type, body)
+    else:
+        print('Invalid Command')
